4b8c2a28f59a61ad13d25fb7f46f5e531c8959f9
Aisar <ahmed.aisar@gmail.com>
2023-10-22 14:52:07 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-// app.js
-process.setMaxListeners(0)
-const cors = require('cors')
-const fs = require('fs')
+const cors = require('cors');
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const chrome = require("chrome-aws-lambda");
@@ -18,88 +16,88 @@ app.get('/', async (req, res) => { res.send({
 });  })
 
 //Anextour api routes
-app.get('/hotels', async (req, res) => { 
-  const cacheKey = 'anex';
-  let cachedData = cache.get(cacheKey);
-  const api = "https://api.anextour.com/search/Hotels?SEARCH_MODE=b2c&SEARCH_TYPE=PACKET_ONLY_HOTELS&lang=&state=maldives&townFrom=1"
+// app.get('/hotels', async (req, res) => { 
+//   const cacheKey = 'anex';
+//   let cachedData = cache.get(cacheKey);
+//   const api = "https://api.anextour.com/search/Hotels?SEARCH_MODE=b2c&SEARCH_TYPE=PACKET_ONLY_HOTELS&lang=&state=maldives&townFrom=1"
 
-  try {
-    const options = {
-      args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
-      executablePath: await chrome.executablePath,
-      headless: "new",
-    };
-    const browser = await chrome.puppeteer.launch(options);
-    const page = await browser.newPage();
+//   try {
+//     const options = {
+//       args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
+//       executablePath: await chrome.executablePath,
+//       headless: "new",
+//     };
+//     const browser = await chrome.puppeteer.launch(options);
+//     const page = await browser.newPage();
 
-    await page.goto(
-      api,
-      {
-        waitUntil: "networkidle0",
-      }
-    );
-    // let html = await page.evaluate(() => {
-    //   return JSON.parse(document.querySelector("body").innerText);
-    // });
-    let body = await page.waitForSelector('body');
-    let json = await body?.evaluate(el => el.textContent);
-    await browser.close();   
-    if (!cachedData) {
-      cache.set(cacheKey, json);
-      cachedData = json;    
-     }
-    res.status(200).json(cachedData);       
-  } catch (error) {
-    console.log(error);
-    await browser.close();   
-    res.statusCode = 500;
-    res.json({
-      body: "Sorry, Something went wrong!",
-    });
-  }
-
-
-})
-
-app.get('/hotel', async (req, res) => { 
-  let query = req.query;
-  const { hotel } = query;
-
-  const api = `https://api.anextour.com/b2c/Hotel?hotel=${hotel}&lang=eng`
-
-  try {
-    const options = {
-      args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
-      executablePath: await chrome.executablePath,
-      headless: "new",
-    };
-    const browser = await chrome.puppeteer.launch(options);
-    const page = await browser.newPage();
-
-    await page.goto(
-      api,
-      {
-        waitUntil: "networkidle0",
-      }
-    );
-    // let html = await page.evaluate(() => {
-    //   return JSON.parse(document.querySelector("body").innerText);
-    // });
-    let body = await page.waitForSelector('body');
-    let json = await body?.evaluate(el => el.textContent);
-    await browser.close();   
-    res.status(200).json(json);       
-  } catch (error) {
-    console.log(error);
-    await browser.close();   
-    res.statusCode = 500;
-    res.json({
-      body: "Sorry, Something went wrong!",
-    });
-  }
+//     await page.goto(
+//       api,
+//       {
+//         waitUntil: "networkidle0",
+//       }
+//     );
+//     // let html = await page.evaluate(() => {
+//     //   return JSON.parse(document.querySelector("body").innerText);
+//     // });
+//     let body = await page.waitForSelector('body');
+//     let json = await body?.evaluate(el => el.textContent);
+//     await browser.close();   
+//     if (!cachedData) {
+//       cache.set(cacheKey, json);
+//       cachedData = json;    
+//      }
+//     res.status(200).json(cachedData);       
+//   } catch (error) {
+//     console.log(error);
+//     await browser.close();   
+//     res.statusCode = 500;
+//     res.json({
+//       body: "Sorry, Something went wrong!",
+//     });
+//   }
 
 
-})
+// })
+
+// app.get('/hotel', async (req, res) => { 
+//   let query = req.query;
+//   const { hotel } = query;
+
+//   const api = `https://api.anextour.com/b2c/Hotel?hotel=${hotel}&lang=eng`
+
+//   try {
+//     const options = {
+//       args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
+//       executablePath: await chrome.executablePath,
+//       headless: "new",
+//     };
+//     const browser = await chrome.puppeteer.launch(options);
+//     const page = await browser.newPage();
+
+//     await page.goto(
+//       api,
+//       {
+//         waitUntil: "networkidle0",
+//       }
+//     );
+//     // let html = await page.evaluate(() => {
+//     //   return JSON.parse(document.querySelector("body").innerText);
+//     // });
+//     let body = await page.waitForSelector('body');
+//     let json = await body?.evaluate(el => el.textContent);
+//     await browser.close();   
+//     res.status(200).json(json);       
+//   } catch (error) {
+//     console.log(error);
+//     await browser.close();   
+//     res.statusCode = 500;
+//     res.json({
+//       body: "Sorry, Something went wrong!",
+//     });
+//   }
+
+
+// })
 
 //Hotelscan api routes
 app.get('/scan', async (req, res) => {
@@ -173,49 +171,49 @@ app.get('/maldives', async (req, res) => {
   
 })
 
-app.post('/autocomplete', async (req, res) => {
-  let query = req.query;
-  const { hotel } = query;
-  // const cacheKey = hotelid;
-  // let cachedData = cache.get(cacheKey);
+// app.post('/autocomplete', async (req, res) => {
+//   let query = req.query;
+//   const { hotel } = query;
+//   // const cacheKey = hotelid;
+//   // let cachedData = cache.get(cacheKey);
 
-  try {
-      const options = {
-        args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
-        executablePath: await chrome.executablePath,
-        headless: "new",
-      };
-      const browser = await chrome.puppeteer.launch(options);
-      const page = await browser.newPage();
-      await page.setRequestInterception(true);
+//   try {
+//       const options = {
+//         args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
+//         executablePath: await chrome.executablePath,
+//         headless: "new",
+//       };
+//       const browser = await chrome.puppeteer.launch(options);
+//       const page = await browser.newPage();
+//       await page.setRequestInterception(true);
  
-      page.once('request', request => {
-          var data = {
-              'method': 'POST',
-              'postData': `query=${hotel}&language=en-us&size=5&pageview_id=&aid=7974605`,
-              'headers': {
-                  ...request.headers(),
-                  'Content-Type': 'application/x-www-form-urlencoded'
-              },
-          };
+//       page.once('request', request => {
+//           var data = {
+//               'method': 'POST',
+//               'postData': `query=${hotel}&language=en-us&size=5&pageview_id=&aid=7974605`,
+//               'headers': {
+//                   ...request.headers(),
+//                   'Content-Type': 'application/x-www-form-urlencoded'
+//               },
+//           };
       
-          request.continue(data);
-          page.setRequestInterception(false);
-      });
-      const response = await page.goto('https://accommodations.booking.com/autocomplete.json');     
-      const responseBody = await response.text();
-      console.log(responseBody);
-      await browser.close();   
-      res.status(200).json(responseBody);           
-    } catch (error) {
-      console.log(error); 
-      res.statusCode = 500;
-      res.json({
-        body: "Sorry, Something went wrong!",
-      });
-    }
+//           request.continue(data);
+//           page.setRequestInterception(false);
+//       });
+//       const response = await page.goto('https://accommodations.booking.com/autocomplete.json');     
+//       const responseBody = await response.text();
+//       console.log(responseBody);
+//       await browser.close();   
+//       res.status(200).json(responseBody);           
+//     } catch (error) {
+//       console.log(error); 
+//       res.statusCode = 500;
+//       res.json({
+//         body: "Sorry, Something went wrong!",
+//       });
+//     }
     
-  });
+//   });
 
 // Start the server
 const PORT = process.env.PORT || 3000;
